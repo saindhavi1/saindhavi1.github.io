34545a034c300d055f535faaa7fa17037f4414aa
Saindhavi Thevathasan <saindhavi076@gmail.com>
2024-08-29 13:23:00 -0400

    final changes

--- js/script.js
+++ js/script.js
@@ -24,4 +24,32 @@ let slider = tns({
             items: 2
         }
     }
-})
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+    const menuLinks = document.querySelectorAll('.menu-buttons a');
+
+    menuLinks.forEach((link, index) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            sections.forEach(section => section.classList.remove('active'));
+            sections[(index)].classList.add('active');
+        });
+    });
+    
+    sections[0].classList.add('active');
+});
